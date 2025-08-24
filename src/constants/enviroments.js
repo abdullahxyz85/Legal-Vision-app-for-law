6e5dcaf74d24b5ac55897dbad4138970713f3be1
abdulmannanclt <abdul.mannan@crewlogix.com>
2025-08-24 15:40:38 +0500

    update url

--- src/constants/enviroments.js
+++ src/constants/enviroments.js
@@ -1,3 +1,3 @@
-const BACKEND_BASE_URL = 'http://localhost:8000'; //import.meta.env.NEXT_PUBLIC_BACKEND_BASE_URL; // process.env.NEXT_PUBLIC_BACKEND_BASE_URL; // import.meta.env.NEXT_PUBLIC_BACKEND_BASE_URL;
+const BACKEND_BASE_URL = 'https://legal-vision-53978b411d1e.herokuapp.com'; //import.meta.env.NEXT_PUBLIC_BACKEND_BASE_URL; // process.env.NEXT_PUBLIC_BACKEND_BASE_URL; // import.meta.env.NEXT_PUBLIC_BACKEND_BASE_URL;
 
 export { BACKEND_BASE_URL };
